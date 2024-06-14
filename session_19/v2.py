7141b729549d03fd354ca9ca24f845907f87bd96
Dhairyashil Ghatage <dhairyashil@Dhairyashils-MacBook-Pro.local>
2024-06-14 17:50:32 +0530

    add transformer block

--- session_19/v2.py
+++ session_19/v2.py
@@ -111,6 +111,21 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
 
+class Block(nn.Module):
+    """ Transformer block: communication followed by comptation"""
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: number of heads
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+
+    def forward(self, x):
+        x = self.sa(x)
+        x = self.ffwd(x)
+        return x
+
 # simple Bigram model
 class BigramLanguageModel(nn.Module):
     def __init__(self):
@@ -118,8 +133,13 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self attention
-        self.ffwd = FeedForward(n_embd)
+        self.blocks = nn.Sequential(
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+            Block(n_embd, n_head=4),
+        )
+        #self.sa_head = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self attention
+        #self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -128,8 +148,7 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) #(B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) #(T,C)
         x = tok_emb + pos_emb  # (B,T,C)
-        x = self.sa_head(x)  #apply one head of self-attention (B,T,C)
-        x = self.ffwd(x) # (B,T,C)
+        x = self.blocks(x) # (B,T,C)
         logits = self.lm_head(x)
 
         if targets is None:
